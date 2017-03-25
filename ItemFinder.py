440310b3596dfaa32dcb73b646ca05c2baa31b82
raolson <rolsonwebdriver@gmail.com>
2017-03-25 16:38:36 -0700

    added functionality to search comments

--- ItemFinder.py
+++ ItemFinder.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import praw
-import pdb
-import re
-import os
+import sys
 
 
 class Finder(object):
@@ -24,35 +22,45 @@ class Finder(object):
                     platform = self.getPlatform(title)
                     itemList = self.getItemList(title)
 
-                    results = self.findByTitle(platform, itemList)
+                    results = self.findAll(platform, itemList)
                     if results:
                         submission.reply(results)
                         repliedContent.append(submission.id)
 
-        # Write IDs of replied posts
-        self.writeReplied(repliedContent)
+                        # Write IDs of replied post and exit
+                        self.writeReplied(repliedContent)
+                        sys.exit()
 
     def writeReplied(self, content):
         with open(self.repliedFile, 'w') as f:
             for postID in content:
                 f.write(postID + '\n')
 
-    def findByTitle(self, platform, itemList):
-        urlList = []
+    def findAll(self, platform, itemList):
+        postList = []
+        commentList = []
         reddit = praw.Reddit('bot1')
         subreddit = reddit.subreddit('rocketleagueexchange')
         for submission in subreddit.new(limit=300):
             title = str(submission.title.encode('utf-8')).lower()
 
             if platform and platform in title:
-                if all(x in title for x in itemList):
-                    urlList.append(self.formatAsLink(title, submission.url))
-        return '\n\n'.join(urlList)
 
-    def formatAsLink(self, title, url):
-        title = title.replace('[', '')
-        title = title.replace(']', '')
-        return '[' + title + '](' + url + ')'
+                # add for title match
+                if all(x in title for x in itemList):
+                    postList.append(self.formatAsLink(title, submission.url))
+
+                # add for comment match
+                for comment in submission.comments.list():
+                        text = comment.body.encode('utf-8').lower()
+                        if all(x in text for x in itemList):
+                            commentList.append(self.formatAsLink(text, submission.url))
+        return 'Posts\n' + '\n\n'.join(postList) + '\nComments\n' + '\n\n'.join(commentList)
+
+    def formatAsLink(self, text, url):
+        text = text.replace('[', '')
+        text = text.replace(']', '')
+        return '[' + text + '](' + url + ')'
 
     def getItemList(self, title):
         """ Get requested item list less platform """
@@ -60,11 +68,11 @@ class Finder(object):
 
     def getPlatform(self, title):
         """ Get platform """
-        if 'xbox' in title.lower():
+        if 'xbox' in title:
                 return 'xbox'
-        elif 'ps4' in title.lower():
+        elif 'ps4' in title:
             return 'ps4'
-        elif 'pc' in title.lower():
+        elif 'pc' in title:
             return 'pc'
         else:
             return None
