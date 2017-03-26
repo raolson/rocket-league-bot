855d5378fd208d528d9e78d5c2c7dc86ef7194c5
raolson <rolsonwebdriver@gmail.com>
2017-03-26 10:56:12 -0700

    Refactor formatting

--- ItemFinder.py
+++ ItemFinder.py
@@ -6,41 +6,57 @@ import sys
 
 class Finder(object):
     def __init__(self):
-        self.repliedFile = "finder_replied.txt"
+        self.repliedFile = 'finder_replied.txt'
+        self.subFind = 'rocketleagueexchange'
+        self.subReply = 'rocketleaguefinder'
+        self.badChars = '[]()'
+        self.botProfile = 'bot1'
 
     def run(self):
+        """
+        Entry point to search subreddit to be summoned
+        """
         # Get replied comment IDs
         repliedContent = self.getReplied()
 
         # Create instance of Reddit
-        reddit = praw.Reddit('bot1')
-        subreddit = reddit.subreddit('rocketleaguefinder')
+        reddit = praw.Reddit(self.botProfile)
+        subreddit = reddit.subreddit(self.subReply)
 
         for submission in subreddit.new(limit=10):
             if submission.id not in repliedContent:
                     title = str(submission.title.encode('utf-8')).lower()
                     platform = self.getPlatform(title)
                     itemList = self.getItemList(title)
-
                     results = self.findAll(platform, itemList)
                     if results:
                         submission.reply(results)
                         repliedContent.append(submission.id)
 
-                        # Write IDs of replied post and exit
+                        # Write IDs of replied post
                         self.writeReplied(repliedContent)
+
+                        """
+                        Exiting after 1 reply per API limit
+                        Profile needs more karma to increase limit
+                        """
                         sys.exit()
 
     def writeReplied(self, content):
+        """ Write exising and new IDs to file """
         with open(self.repliedFile, 'w') as f:
             for postID in content:
                 f.write(postID + '\n')
 
     def findAll(self, platform, itemList):
+        """
+        Find all occurances in posts and comments
+        Return formatted response
+        """
         postList = []
         commentList = []
-        reddit = praw.Reddit('bot1')
-        subreddit = reddit.subreddit('rocketleagueexchange')
+        reddit = praw.Reddit(self.botProfile)
+        subreddit = reddit.subreddit(self.subFind)
         for submission in subreddit.new(limit=300):
             title = str(submission.title.encode('utf-8')).lower()
 
@@ -55,19 +71,25 @@ class Finder(object):
                         text = comment.body.encode('utf-8').lower()
                         if all(x in text for x in itemList):
                             commentList.append(self.formatAsLink(text, submission.url))
-        return 'Posts\n' + '\n\n'.join(postList) + '\nComments\n' + '\n\n'.join(commentList)
+        response = self.formatResponse(self.postList, self.commentList)
+        return response
 
     def formatAsLink(self, text, url):
-        text = text.replace('[', '')
-        text = text.replace(']', '')
+        """ Remove special chars and format as Reddit URL link """
+        for c in self.badChars:
+            text = text.replace(c, '')
         return '[' + text + '](' + url + ')'
+
+    def formatResponse(self, posts, comments):
+        """ Format Bot response for readability """
+        return 'Posts\n' + '\n\n'.join(posts) + '\n\nComments\n' + '\n\n'.join(comments)
 
     def getItemList(self, title):
         """ Get requested item list less platform """
         return title.lower().split('-')[1:]
 
     def getPlatform(self, title):
-        """ Get platform """
+        """ Get platform by title"""
         if 'xbox' in title:
                 return 'xbox'
         elif 'ps4' in title:
